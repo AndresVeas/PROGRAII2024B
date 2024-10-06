c3f9907bbc7b0c10d9e7b3635399360f2bd14786
AndresVeas <andres.veas@epn.edu.ec>
2024-10-06 11:22:52 -0500

    Configuración de Tarea 01

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Estoy harto");
+        System.out.println("TAREA 01");
+        System.out.println("Nombre: Andrés Enrique Veas Loor \nCurso: GR2SW \nFecha:06/10/2024");
+        System.out.println("Cambios realizados en el Visual code: \n-Cambio de tema predeterminado\n-Personalización terminal");
+        System.out.println("-Sinctronización de Git, GitHub con VSC.\n-Personalización de colores en la fuente del editor");
+        //Aún no sé como borrar la primera línea de los directorios 
+        /*
+         * En la captura se muestra la parte de los directorios (a la izquierda) con la finalidad de mostrar la creación de un espacio
+         * de trabajo con un JAVA PROYECT. De igual manera la funcionalidad de GitHub Activa (M:modified).
+         * En el momento de programar se aplicarán dos comandos: ctrl + B y ctrl + ñ para dejar la pantalla limpia
+         */
+
     }
 }
