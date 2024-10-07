2adb3a8aa15bdf6f0bf789eff59941b14442349f
AndresVeas <andres.veas@epn.edu.ec>
2024-10-07 15:48:10 -0500

    Hola

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ public class App {
         System.out.println("Nombre: Andrés Enrique Veas Loor \nCurso: GR2SW \nFecha:06/10/2024");
         System.out.println("Cambios realizados en el Visual code: \n-Cambio de tema predeterminado\n-Personalización terminal");
         System.out.println("-Sinctronización de Git, GitHub con VSC.\n-Personalización de colores en la fuente del editor");
-        //Aún no sé como borrar la primera línea de los directorios 
         /*
          * En la captura se muestra la parte de los directorios (a la izquierda) con la finalidad de mostrar la creación de un espacio
          * de trabajo con un JAVA PROYECT. De igual manera la funcionalidad de GitHub Activa (M:modified).
